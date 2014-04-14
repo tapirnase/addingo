aa097bc8f99eeef3e0b977cd0adb10d43c6cf453
tapirnase <tobias.arndt@rocketmail.com>
2014-04-14 06:01:53 +0200

    old localstirge bug - typo

--- javascript/app.js
+++ javascript/app.js
@@ -45,7 +45,7 @@ function(Backbone, RiddleController, Observer, Templates, MapModel, MapCollectio
       for(var i = 0; i < maps.length; i++)  {
         var mapkey = localStorage.get_mapkey(maps[i].map);
         if(localStorage.getItem(mapkey) == null)  {
-          localStorage.setItem(mapkey) = JSON.stringify({tile: 0, highscore: 0});
+          localStorage.setItem(mapkey, JSON.stringify({tile: 0, highscore: 0}));
         }
         models.push(new MapModel(maps[i]));
       }
